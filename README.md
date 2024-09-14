def881fda02084d066b869fe3a59b455241ee68c
Anubhav Maurya <anubhavmaurya@example.com>
2024-09-14 11:50:45 +0530

    knapsacktop down

--- README.md
+++ README.md
@@ -1,8 +1,17 @@
 There are three type of knapsack 
-1. Fractional Knap
+1. Fractional Knap ---> greedy method 
 2. 0/1 Knapsack
 3. Unbounded Knapsack
 
+
+Part-1 
+[0/1 Knapsack]
+
+in knapsack either take or not take in both case item is processed. 
+ 
+after processed never include in knapsack. 
+
+Questions:
 
 
 
@@ -10,4 +19,11 @@ There are three type of knapsack
 
 Part-2 
 Unbounded Knapsack
+if not take then it is processed. 
 
+if take then it will not processed. 
+Questions:
+1. Rod Cutting. 
+2. Coin Change -1
+3. Coin Change -2
+4. Maximum Ribbon Cut. 
--- src/KnapsackTopDown.java
+++ src/KnapsackTopDown.java
@@ -1,5 +1,39 @@
+import java.util.Arrays;
+
 public class KnapsackTopDown {
-    public static void main(String[] args) {
         
+    static int[][] tabel;
+
+    static {
+        // Initialize the table for memoization
+        tabel = new int[4][5];
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (i==0 || j == 0) {
+                    tabel[i][j] = 0;
+                }
+            }
+        }
+    }
+    public static int kstpdwn(int[] wt, int[] val, int weight, int length){
+        if (wt[length-1]<= weight) {
+            
+            tabel[length][weight] = Math.max(val[length-1]+tabel[length-1][weight-wt[length-1]], tabel[length-1][weight]);
+
+            return tabel[length][weight];
+        }
+        tabel[length][weight] = tabel[length-1][weight];
+        return tabel[length][weight];
+
+    }
+    public static void main(String[] args) {
+        int profit[] = { 1, 2, 3 };
+        int weight[] = { 4, 5, 1 };
+        int N = 3; // Number of items
+        int W = 4; // Capacity of the knapsack
+
+        System.out.println(kstpdwn(weight, profit, W, N));
+
+
     }
 }
